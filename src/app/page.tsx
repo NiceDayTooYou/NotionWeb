f7d57e283fdc23dde3d888a9f465d4235bfb7e93
NiceDayTooYou <88866636+NiceDayTooYou@users.noreply.github.com>
2023-08-22 21:18:11 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,13 @@ export const metadata = {
 };
 
 export default function HomePage() {
-  return <h1 className="mt-12 text-center text-3xl font-bold">If You Don't Know, Now You Know</h1>;
+  return <h1 className="mt-12 text-center text-3xl font-bold">If You Don't Know Now You Know</h1>;
 }
+
+<p className="text-center text-lg mt-4">
+  Welcome to Li Yong's personal page. Explore my journey and insights.
+  Check out this <a href="https://s1.ax1x.com/2023/08/20/pP8bBkt.png">
+    <img src="https://s1.ax1x.com/2023/08/20/pP8bBkt.png" alt="Description of the image" className="w-32 h-32 rounded-full" />
+  </a> from my recent adventure.
+</p>
+
