7959f402c3a9d4113d0afc55f7f54b3319cd5046
NiceDayTooYou <88866636+NiceDayTooYou@users.noreply.github.com>
2023-08-22 22:48:34 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,10 @@ export default function HomePage() {
   return (
     <div>
       <h1 className="mt-12 text-center text-3xl font-bold">Welcome to my page</h1>
-      <h2 className="mt-4 text-center text-2xl font-semibold">Second Item Title</h2>
-      <p className="text-center">This is the content for the second item.</p>
-      <h2 className="mt-4 text-center text-2xl font-semibold">Third Item Title</h2>
-      <p className="text-center">This is the content for the third item.</p>
+      <h2 className="mt-4 text-center text-2xl font-semibold">How are you today</h2>
+      <p className="text-center">热爱生活,知足常乐.</p>
+      <h2 className="mt-4 text-center text-2xl font-semibold">Enjoy your time</h2>
+      <p className="text-center">昨天已经成为了历史,而每天却是个谜团,今天是上天赐予的礼物,要像珍惜礼物那样珍惜今天.</p>
     </div>
   );
 }
