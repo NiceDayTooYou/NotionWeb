ed24bd4d842c0423737e2e84f9300f3c9529fab3
NiceDayTooYou <88866636+NiceDayTooYou@users.noreply.github.com>
2023-08-22 22:39:36 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,16 @@ export const metadata = {
   title: 'Li Yong | 和美好不期而遇',
 };
 
+import React from 'react';
+
 export default function HomePage() {
   return (
     <div>
       <h1 className="mt-12 text-center text-3xl font-bold">Welcome to my page</h1>
-      <h2 className="mt-4 text-center text-2xl font-semibold">If You don't know now you know</h2>
-      <p className="text-center">This is the second item's content.</p>
-      <h2 className="mt-4 text-center text-2xl font-semibold">How are you today</h2>
-      <p className="text-center">This is the third item's content.</p>
+      <h2 className="mt-4 text-center text-2xl font-semibold">Second Item Title</h2>
+      <p className="text-center">This is the content for the second item.</p>
+      <h2 className="mt-4 text-center text-2xl font-semibold">Third Item Title</h2>
+      <p className="text-center">This is the content for the third item.</p>
     </div>
   );
 }
