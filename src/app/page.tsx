1bab10973ca5b128b8af1a09dbb3bbdcd14656ee
NiceDayTooYou <88866636+NiceDayTooYou@users.noreply.github.com>
2023-08-22 22:34:57 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,13 @@ export const metadata = {
 };
 
 export default function HomePage() {
-  return <h1 className="mt-12 text-center text-3xl font-bold">Li Yong</h1>;
+ return (
+    <div>
+      <h1 className="mt-12 text-center text-3xl font-bold">Welcome to my page</h1>
+      <h2 className="mt-4 text-center text-2xl font-semibold">If You don't know now you know</h2>
+      <p className="text-center">This is the second item's content.</p>
+      <h2 className="mt-4 text-center text-2xl font-semibold">How are you today</h2>
+      <p className="text-center">This is the third item's content.</p>
+    </div>
+  );
 }
