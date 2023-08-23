78baacf01595f53b6a3928c005d96e34980f16ef
NiceDayTooYou <88866636+NiceDayTooYou@users.noreply.github.com>
2023-08-23 20:49:01 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-export const metadata = {
-  title: 'Li Yong | 和美好不期而遇',
-};
-
 import React from 'react';
 
 export default function HomePage() {
@@ -9,9 +5,13 @@ export default function HomePage() {
     <div>
       <h1 className="mt-12 text-center text-3xl font-bold">Welcome to my page</h1>
       <h2 className="mt-4 text-center text-2xl font-semibold">How are you today</h2>
-      <p className="text-center">热爱生活,知足常乐.</p>
+      <p className="text-center" style={{ color: 'green', fontSize: '18px' }}>
+        热爱生活，知足常乐。
+      </p>
       <h2 className="mt-4 text-center text-2xl font-semibold">Enjoy your time</h2>
-      <p className="text-center">昨天已经成为了历史,明天却是个谜团,而今天是上天赐予的礼物,要像珍惜礼物那样珍惜今天.</p>
+      <p className="text-center" style={{ color: 'blue', fontSize: '18px' }}>
+        昨天已经成为了历史，明天却是个谜团，而今天是上天赐予的礼物，要像珍惜礼物那样珍惜今天.
+      </p>
     </div>
   );
 }
