84345b621b1b82e6f3f16e4c79b1de37733a80e8
NiceDayTooYou <88866636+NiceDayTooYou@users.noreply.github.com>
2023-08-23 21:08:01 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,18 @@ export default function HomePage() {
     <div>
       <h1 className="mt-12 text-center text-3xl font-bold">Welcome to my page</h1>
       <p className="text-center" style={{ color: 'red', fontSize: '30px' }}>
-        和美好不期而遇
+        和美好不期而遇❤️
       </p> {/* 添加一个闭合标签 */}
       <h2 className="mt-4 text-center text-2xl font-semibold">How are you today</h2>
       <p className="text-center" style={{ color: 'green', fontSize: '30px' }}>
-        热爱生活，知足常乐。
+        热爱生活，知足常乐👻
       </p>
       <h2 className="mt-4 text-center text-2xl font-semibold">Enjoy your time</h2>
       <p className="text-center" style={{ color: 'blue', fontSize: '30px' }}>
-        昨天已经成为了历史，明天却是个谜团，而今天是上天赐予的礼物，要像珍惜礼物那样珍惜今天.
+        昨天已经成为历史
+        明天却是个谜团
+        而今天是天赐的礼物
+        要像珍惜礼物那样珍惜今天
       </p>
     </div>
   );
