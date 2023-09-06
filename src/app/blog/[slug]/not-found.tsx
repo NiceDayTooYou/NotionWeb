aa195004ca89facb97e7e49334febfce561e92b1
NiceDayTooYou <88866636+NiceDayTooYou@users.noreply.github.com>
2023-09-06 08:50:10 +0800

    Update not-found.tsx

--- src/app/blog/[slug]/not-found.tsx
+++ src/app/blog/[slug]/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div className="mx-auto mt-40 text-center">
       <h2 className="mb-4 text-3xl font-bold">Post Not Found</h2>
-      <Link href="/serendipity">
+      <Link href="/blog">
         <span className="mr-2">&larr;</span>
         <span>Go to list page</span>
       </Link>
