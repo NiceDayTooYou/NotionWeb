58271ef300a6cb26064f473d4da13aafbeb4cc8c
NiceDayTooYou <88866636+NiceDayTooYou@users.noreply.github.com>
2023-09-07 22:15:57 +0800

    Update index.ts

--- src/components/header/index.ts
+++ src/components/header/index.ts
@@ -1,8 +1,6 @@
-export { default } from './header';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './_custom-styles.scss'; // 引入全局样式文件
-import Header from './Header'; // 导入主要组件
+import Header from './header'; // 导入主要组件
 
 ReactDOM.render(<Header />, document.getElementById('root'));
