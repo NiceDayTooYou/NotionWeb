8564cccd03f6752fb67d4a7624696aa4745fe1e0
NiceDayTooYou <88866636+NiceDayTooYou@users.noreply.github.com>
2023-09-18 19:34:09 +0800

    Update _app.js

--- _app.js
+++ _app.js
@@ -1,14 +1,13 @@
 import '../src/styles/globals.css';
-
 import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    const audio = new Audio('/studying.mp3');
-    audio.play();
-  }, []);
+  useEffect(() => {
+    const audio = new Audio('/studying.mp3');
+    audio.play();
+  }, []);
 
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
