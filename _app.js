dd9f2ea4fa177b403dea24a32af348e2d7fe50e5
NiceDayTooYou <88866636+NiceDayTooYou@users.noreply.github.com>
2023-09-18 19:56:56 +0800

    Update _app.js

--- _app.js
+++ _app.js
@@ -1,22 +1,5 @@
 import '../src/styles/globals.css';
-import { useEffect, useRef } from 'react';
-
 function MyApp({ Component, pageProps }) {
-  const audioRef = useRef(null);
-
-  const playAudio = () => {
-    if (audioRef.current) {
-      audioRef.current.play();
-    }
-  };
-
-  return (
-    <div>
-      <button onClick={playAudio}>Play Audio</button>
-      <audio ref={audioRef} src="/studying.mp3" />
-      <Component {...pageProps} />
-    </div>
-  );
+  return <Component {...pageProps} />;
 }
-
-export default MyApp;
+export default MyApp; 
